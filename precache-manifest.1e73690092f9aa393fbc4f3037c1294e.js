049a67164ad41ccf657628a7885dcf8ebe22c033
tejasraibagi26 <tejasraibagi@gmail.com>
2020-05-15 15:59:16 +0530

    Updates

---
+++ precache-manifest.1e73690092f9aa393fbc4f3037c1294e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462895bfa3d7fc46f888316159c2d538",
+    "url": "/tejasraibagi26.github.io/info/index.html"
+  },
+  {
+    "revision": "0cc021933d0bf1dd506b",
+    "url": "/tejasraibagi26.github.io/info/static/css/2.4b36e02c.chunk.css"
+  },
+  {
+    "revision": "47f230dc09ccb565b9c2",
+    "url": "/tejasraibagi26.github.io/info/static/css/main.bd062357.chunk.css"
+  },
+  {
+    "revision": "0cc021933d0bf1dd506b",
+    "url": "/tejasraibagi26.github.io/info/static/js/2.0b4e7a7e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tejasraibagi26.github.io/info/static/js/2.0b4e7a7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f230dc09ccb565b9c2",
+    "url": "/tejasraibagi26.github.io/info/static/js/main.ce7a5871.chunk.js"
+  },
+  {
+    "revision": "bf4d45ec14a258415a4e",
+    "url": "/tejasraibagi26.github.io/info/static/js/runtime-main.8b86e24c.js"
+  },
+  {
+    "revision": "bac69a2486bc610c16da3c36a01134a2",
+    "url": "/tejasraibagi26.github.io/info/static/media/cert.bac69a24.svg"
+  },
+  {
+    "revision": "539537414b45617e3771e5d515cf4c07",
+    "url": "/tejasraibagi26.github.io/info/static/media/introImage.53953741.svg"
+  },
+  {
+    "revision": "9bb1c583844c5783912decbd63168017",
+    "url": "/tejasraibagi26.github.io/info/static/media/logo.9bb1c583.svg"
+  },
+  {
+    "revision": "393207e15ce6676d524906b59e69ef1f",
+    "url": "/tejasraibagi26.github.io/info/static/media/skill.393207e1.svg"
+  }
+]);
